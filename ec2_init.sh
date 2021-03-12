8e8f3b9180ca10464500c491486914db630c6be9
NAKAMURA Shingo <git@itrev.info>
2021-03-12 15:28:26 +0900

    ec2_init.sh修正

--- ec2_init.sh
+++ ec2_init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /root/ec2settings
+cd /root/init
 
 # パッケージ追加
 yum -y groupinstall Base "Development Tools"
@@ -14,17 +14,17 @@ ln -sf /usr/share/zoneinfo/Japan /etc/localtime
 sed -i "s/\"UTC\"/\"Japan\"/g" /etc/sysconfig/clock
 
 # 文字コード設定
-#sed -i "s/en_US\.UTF-8/ja_JP\.UTF-8/g" /etc/sysconfig/i18n
-#echo "locale: ja_JP.UTF-8" >> /etc/cloud/cloud.cfg
+sed -i "s/en_US\.UTF-8/ja_JP\.UTF-8/g" /etc/sysconfig/i18n
+echo "locale: ja_JP.UTF-8" >> /etc/cloud/cloud.cfg
 
 # .*rcファイルなど取得＆設定
 TGZ=ec2_init.tgz
-#wget https://be0.fit/$TGZ
+#wget https://ec2.itrev.info/$TGZ
 tar xvfz $TGZ
 /bin/cp ec2/skel/.*rc /root
 /bin/cp ec2/skel/.*rc /home/ec2-user
 /bin/cp ec2/skel/.*rc /etc/skel
-#echo "export LANG=ja_JP.UTF-8" >> /root/.bash_profile
+echo "export LANG=ja_JP.UTF-8" >> /root/.bash_profile
 
 # ツール等コピー
 /bin/cp ec2/usr/local/bin/* /usr/local/bin
@@ -34,4 +34,4 @@ cat ec2/authorized_keys >> /home/ec2-user/.ssh/authorized_keys
 chown -R ec2-user:ec2-user /home/ec2-user
 
 # 後始末
-#rm -rf ec2*
+rm -rf ec2
