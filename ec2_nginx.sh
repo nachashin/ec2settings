75ccd44657145f909899bb19ca6028d0d24acdc8
NAKAMURA Shingo <git@itrev.info>
2021-03-13 11:32:41 +0900

    ec2_nginx.sh修正

--- ec2_nginx.sh
+++ ec2_nginx.sh
@@ -2,6 +2,6 @@
 amazon-linux-extras install -y nginx1.12
 systemctl enable nginx
 systemctl start nginx
-wget https://be0.fit/ec2_nginx.html
+#wget https://be0.fit/ec2_nginx.html
 /bin/cp ec2_nginx.html /usr/share/nginx/html/index.html
-rm -rf ec2*
+#rm -rf ec2*
